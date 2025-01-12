17b299645ed42c6a743005305f5a419e708adb46
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-01-12 19:02:48 -0300

    :sparkles: feat: Verificação de 5 lances por usuário

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -20,6 +20,13 @@ class Leilao
         if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
             return;
         }
+
+        $totalLancesUsuario = $this->quantidadeLancesPorUsuario($lance->getUsuario());
+
+        if ($totalLancesUsuario >= 5) {
+            return;
+        }
+
         $this->lances[] = $lance;
     }
 
@@ -27,6 +34,19 @@ class Leilao
     {
         $ultimoLance = $this->lances[array_key_last($this->lances)];
         return $lance->getUsuario() == $ultimoLance->getUsuario();
+    }
+
+    private function quantidadeLancesPorUsuario(Usuario $usuario): int
+    {
+        $totalLancesUsuario = array_reduce($this->lances, function (int $totalAcumulado, Lance $lanceAtual) use ($usuario): int {
+            if ($lanceAtual->getUsuario() == $usuario) {
+                return $totalAcumulado + 1;
+            }
+
+            return $totalAcumulado;
+        }, 0);
+
+        return $totalLancesUsuario;
     }
 
     /**
