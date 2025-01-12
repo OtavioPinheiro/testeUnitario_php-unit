4a7bbb23deea9edbd1be0afc7bf26bc052247b66
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2025-01-12 11:43:02 -0300

    :sparkles: feat: LeilaoTest

---
+++ tests/Model/LeilaoTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Alura\Leilao\Tests\Model;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoTest extends TestCase
+{
+    /**
+     * @dataProvider geraLances
+     */
+    public function testLeilaoDeveReceberLances(int $qtdeLances, Leilao $leilao, array $valores): void
+    {
+        self::assertCount($qtdeLances, $leilao->getLances());
+
+        foreach ($valores as $i => $valorEsperado) {
+            self::assertEquals($valorEsperado, $leilao->getLances()[$i]->getValor());
+        }
+    }
+
+    public function testLeilaoNaoDeveReceberLancesRepetidos(): void
+    {
+        $leilao = new Leilao('Variante 0KM');
+        $ana = new Usuario('Ana');
+
+        $leilao->recebeLance(new Lance($ana, 1000));
+        $leilao->recebeLance(new Lance($ana, 1500));
+
+        static::assertCount(1, $leilao->getLances());
+        static::assertEquals(1000, $leilao->getLances()[0]->getValor());
+    }
+
+    public static function geraLances(): array
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilaoCom2Lances = new Leilao('Fiat 147 0KM');
+        $leilaoCom2Lances->recebeLance(new Lance($joao, 1000));
+        $leilaoCom2Lances->recebeLance(new Lance($maria, 2000));
+
+        $leilaoCom1Lance = new Leilao('Fusca 1972 0KM');
+        $leilaoCom1Lance->recebeLance(new Lance($maria, 5000));
+
+        return [
+            "Leilão com dois lances" => [2, $leilaoCom2Lances, [1000, 2000]],
+            "Leilão com um lance" => [1, $leilaoCom1Lance, [5000]]
+        ];
+    }
+}
